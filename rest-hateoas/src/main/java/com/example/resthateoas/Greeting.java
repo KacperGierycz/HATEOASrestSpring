47f933d3c664e5f0deb12d980e3969bf944916dc
Kacper <kacpergierycz@gmail.com>
2021-05-06 11:59:10 +0200

    done :) have to merge mine solution to bug on spring tutorial

--- rest-hateoas/src/main/java/com/example/resthateoas/Greeting.java
+++ rest-hateoas/src/main/java/com/example/resthateoas/Greeting.java
@@ -8,16 +8,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Greeting extends RepresentationModel<Greeting> {
 
 	private final String content;
-	
+
 	@JsonCreator
 	public Greeting(@JsonProperty("content") String content) {
-		
-		this.content=content;
+		this.content = content;
 	}
-	
+
 	public String getContent() {
 		return content;
 	}
-
 }
-
